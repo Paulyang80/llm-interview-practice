0c2bd3044921bc9164e1c8a7f32a2388280fbf7f
Paul Yang <paulyang80@gmail.com>
2026-01-01 16:34:46 -0500

    Practice Day 1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// http://localhost:5173
+
 // function App() {
 //   const [count, setCount] = useState(0)
 
@@ -33,12 +35,69 @@ import './App.css'
 // }
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function handleAdd() {
+    setCount(count + 1);
+  }
+
+  function handleSub() {
+    setCount(count - 1);
+  }
+
+  function reset() {
+    setCount(0);
+  }
+
   return (
     <main style={{ padding: 24 }}>
       <h1>我的第一個 Vite 網頁</h1>
       <p>接下來我會在這裡做介面。</p>
+      <ActionButton onClick={handleAdd}>點我加 1</ActionButton>
+      <p>count: {count}</p>
+      <ActionButton onClick={handleSub}>點我減 1</ActionButton>
+      <ActionButton onClick={reset}>重設</ActionButton>
+      <p>
+        {count % 2 === 0 ? '偶數 ✨' : '奇數 🔥'}
+      </p>
+      <Card>
+        <h2>這是卡片標題</h2>
+        <p>這是卡片內容。</p>
+      </Card>
     </main>
   )
 }
+
+function ActionButton({onClick, children}: { onClick: () => void, children: React.ReactNode }) {
+  return (
+    <button onClick={onClick}>
+      {children}
+    </button>
+  );
+}
+
+function Card({children}: { children: React.ReactNode }) {
+  return (
+    <div className="card">
+      {children}
+    </div>
+  );
+}
+
+// function AddButton({ onClick }: { onClick: () => void }) {
+//   return (
+//     <button onClick={onClick}>
+//       點我加 1
+//     </button>
+//   );
+// }
+
+// function SubButton({ onClick }: { onClick: () => void }) {
+//   return (
+//     <button onClick={onClick}>
+//       點我減 1
+//     </button>
+//   );
+// }
 
 export default App
